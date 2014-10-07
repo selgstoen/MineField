3aa1b0653423e8661178c572ac67e68fe9a977f2
Svein Elgstøen <svein.elgstoen@statkraft.com>
2014-10-07 21:50:14 +0200

    Created cell model

--- MineField/scripts/models.js
+++ MineField/scripts/models.js
@@ -1,13 +1,30 @@
 ﻿define(["require", "exports", "bombservice", "knockout"], function(require, exports, bs, ko) {
+    //export function cell(rowNumber: number, columnNumber: number) {
+    //    var self = this;
+    //    self.rowNumber = rowNumber;
+    //    self.columnNumber = columnNumber;
+    //    self.isBomb = false;
+    //    self.numberOfCloseBombs = 0;
+    //}
     function row(rowNumber, columnCount) {
         var self = this;
         self.cells = ko.observableArray();
 
         for (var cellNr = 0; cellNr < columnCount; cellNr++) {
-            self.cells.push('row #' + rowNumber + 'cell #' + cellNr);
+            //self.cells.push('row #' + rowNumber + 'cell #' + cellNr);
+            self.cells.push(new exports.cell(rowNumber, cellNr));
         }
     }
     exports.row = row;
+
+    function cell(rowNumber, columnNumber) {
+        var self = this;
+        self.rowNumber = rowNumber;
+        self.columnNumber = columnNumber;
+        self.isBomb = false;
+        self.numberOfCloseBombs = 0;
+    }
+    exports.cell = cell;
 
     function field(rowCount, columnCount, numberOfBombs) {
         var self = this;
--- MineField/scripts/models.ts
+++ MineField/scripts/models.ts
@@ -1,13 +1,34 @@
 ﻿import bs = require("bombservice");
 import ko = require("knockout");
 
+//export function cell(rowNumber: number, columnNumber: number) {
+//    var self = this;
+//    self.rowNumber = rowNumber;
+//    self.columnNumber = columnNumber;
+//    self.isBomb = false;
+//    self.numberOfCloseBombs = 0;
+
+
+//}
+
 export function row(rowNumber: number, columnCount: number) {
     var self = this;
     self.cells = ko.observableArray();
 
     for (var cellNr = 0; cellNr < columnCount; cellNr++) {
-        self.cells.push('row #' + rowNumber + 'cell #' + cellNr);
+        //self.cells.push('row #' + rowNumber + 'cell #' + cellNr);
+        self.cells.push(new cell(rowNumber, cellNr));
     }
+}
+
+export function cell(rowNumber: number, columnNumber: number) {
+    var self = this;
+    self.rowNumber = rowNumber;
+    self.columnNumber = columnNumber;
+    self.isBomb = false;
+    self.numberOfCloseBombs = 0;
+
+
 }
 
 export function field(rowCount: number, columnCount: number, numberOfBombs: number) {
