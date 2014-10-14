8f7163de805266d1e98996fd7155a19aaa2ff53b
Svein Elgstøen <svein.elgstoen@statkraft.com>
2014-10-14 23:53:36 +0200

    Added randomizing bomb positions

--- MineField/scripts/bombservice.js
+++ MineField/scripts/bombservice.js
@@ -2,8 +2,33 @@
     function getBombPositions(numberOfBombs, numberOfRows, numberOfCoumns) {
         var bombPositions = [];
 
-        for (var i = 0; i < numberOfBombs; i++) {
-            bombPositions.push(new models.bombPosition(i, i));
+        var bombPositionAlreadyExists = function (position) {
+            for (var i = 0; i < bombPositions.length; i++) {
+                var existingBombPosition = bombPositions[i];
+                if (existingBombPosition.rowIndex == position.rowIndex && existingBombPosition.columnIndex == position.columnIndex)
+                    return true;
+            }
+
+            return false;
+        };
+
+        var getRandomIndex = function (rangeEnd) {
+            return Math.floor(Math.random() * rangeEnd + 1);
+        };
+
+        var getBombPosition = function (rowCount, columnCount) {
+            var rowIndex = getRandomIndex(rowCount);
+            var columnIndex = getRandomIndex(columnCount);
+            return new models.bombPosition(rowIndex, columnIndex);
+        };
+
+        while (bombPositions.length < numberOfBombs) {
+            var bombPosition = getBombPosition(numberOfRows, numberOfCoumns);
+            if (!bombPositionAlreadyExists(bombPosition)) {
+                bombPositions.push(bombPosition);
+            } else {
+                break;
+            }
         }
 
         return bombPositions;
--- MineField/scripts/bombservice.ts
+++ MineField/scripts/bombservice.ts
@@ -3,9 +3,36 @@
 export function getBombPositions(numberOfBombs: number, numberOfRows: number, numberOfCoumns: number) {
     var bombPositions = [];
 
-    for (var i = 0; i < numberOfBombs; i++) {
-        bombPositions.push(new models.bombPosition(i, i));
+
+    var bombPositionAlreadyExists = function (position) {
+         for (var i = 0; i < bombPositions.length; i++) {
+             var existingBombPosition = bombPositions[i];
+             if (existingBombPosition.rowIndex == position.rowIndex && existingBombPosition.columnIndex == position.columnIndex)
+                 return true;
+         }
+
+        return false;
+    };
+
+    var  getRandomIndex = function(rangeEnd: number) : number {
+        return Math.floor( Math.random() * rangeEnd + 1);
+    }
+
+    var getBombPosition = function (rowCount: number, columnCount: number) {
+        var rowIndex = getRandomIndex(rowCount);
+        var columnIndex = getRandomIndex(columnCount);
+        return new models.bombPosition(rowIndex, columnIndex);
+    }
+
+    while (bombPositions.length < numberOfBombs) {
+        var bombPosition = getBombPosition(numberOfRows, numberOfCoumns);
+        if (!bombPositionAlreadyExists(bombPosition)) {
+            bombPositions.push(bombPosition);
+        } else {
+            break;
+        }
     }
 
     return bombPositions;
 }
+
